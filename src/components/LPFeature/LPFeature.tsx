fd83b17392a51fb447f58b170e8dfe55c4788b14
Andreas Schmidt <andreas.schmidt.lth@proton.me>
2025-09-18 14:02:47 +0200

    fixed LPFeature according to lint

--- src/components/LPFeature/LPFeature.tsx
+++ src/components/LPFeature/LPFeature.tsx
@@ -1,48 +1,55 @@
 // components/LPFeature/LPFeature.tsx
-// @ts-ignore
 import React from 'react'
 import './LPFeature.css'
 
 export type LPFeatureProps = {
-    emoji: string
-    title: string
-    description: string
-    onClick?: () => void
-    as?: 'button' | 'article' | 'a'
-    href?: string // nur relevant, wenn as="a"
+  emoji: string
+  title: string
+  description: string
+  onClick?: () => void
+  as?: 'button' | 'article' | 'a'
+  href?: string // nur relevant, wenn as="a"
 }
 
 export default function LPFeature({
-                                      emoji,
-                                      title,
-                                      description,
-                                      onClick,
-                                      as = 'article',
-                                      href
+                                    emoji,
+                                    title,
+                                    description,
+                                    onClick,
+                                    as = 'article',
+                                    href,
                                   }: LPFeatureProps) {
-    const Tag: any = as
-
-    const commonProps: any = {
-        className: 'ml-lpfeature',
-        onClick,
-    }
-
-    if (as === 'a' && href) {
-        commonProps.href = href
-    }
-    if (as === 'button') {
-        commonProps.type = 'button'
-    }
-
-    return (
-        <Tag {...commonProps}>
-      <span className="ml-lpfeature__emoji" aria-hidden="true">
+  const content = (
+    <>
+      <span className="ml-lpfeature-emoji" aria-hidden="true">
         {emoji}
       </span>
-            <div className="ml-lpfeature__body">
-                <h3 className="ml-lpfeature__title">{title}</h3>
-                <p className="ml-lpfeature__desc">{description}</p>
-            </div>
-        </Tag>
+      <div className="ml-lpfeature-body">
+        <h3 className="ml-lpfeature-title">{title}</h3>
+        <p className="ml-lpfeature-desc">{description}</p>
+      </div>
+    </>
+  )
+
+  if (as === 'a') {
+    return (
+      <a className="ml-lpfeature" onClick={onClick} href={href ?? '#'}>
+        {content}
+      </a>
     )
+  }
+
+  if (as === 'button') {
+    return (
+      <button className="ml-lpfeature" onClick={onClick} type="button">
+        {content}
+      </button>
+    )
+  }
+
+  return (
+    <article className="ml-lpfeature" onClick={onClick}>
+      {content}
+    </article>
+  )
 }
